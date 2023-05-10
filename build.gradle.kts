063302bf92c5e0cb544bf449a3d9a4da32306b79
İbrahim Aydın <55055268+ibrahimaydindev@users.noreply.github.com>
2023-05-10 17:26:22 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,7 @@ dependencies {
     implementation ("io.grpc:grpc-kotlin-stub:1.3.0")
     implementation platform("com.google.firebase:firebase-bom:24.4.5")
     implementation ("com.google.firebase:firebase-firestore-ktx:24.4.5")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+  
 }
 
 publishing {
